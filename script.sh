ad97f47671d6ca2917d021e6667640a00ac513f0
Dmitrii Babich <bbchd@amazon.com>
2021-01-13 19:11:15 +0100

    Fix message

--- script.sh
+++ script.sh
@@ -1,14 +1,18 @@
+# wget -O - https://raw.githubusercontent.com/dmitrybabich/asus-parental-control-switcher/master/script.sh | bash
+# sudo su -c "bash <(wget -qO- https://raw.githubusercontent.com/dmitrybabich/asus-parental-control-switcher/master/script.sh)" root
+
+# wget -O https://raw.githubusercontent.com/dmitrybabich/asus-parental-control-switcher/master/script.sh
 ENABLED=`nvram get MULTIFILTER_ENABLE`
 echo $ENABLED
 if [ $ENABLED = "2" ]; then
-    echo "Disabling"
-    nvram set MULTIFILTER_ALL="1"
-    nvram set MULTIFILTER_MACFILTER_DAYTIME="<"
+    echo "Disabling PC"
+    nvram set MULTIFILTER_ALL=""
+    nvram set MULTIFILTER_MACFILTER_DAYTIME=""
     nvram set MULTIFILTER_DEVICENAME=""
     nvram set MULTIFILTER_ENABLE=""
     nvram set MULTIFILTER_MAC=""
 else
-    echo "Enabling"
+    echo "Enabling PC"
     nvram set MULTIFILTER_ALL="1"
     nvram set MULTIFILTER_MACFILTER_DAYTIME="<"
     nvram set MULTIFILTER_DEVICENAME="XBOX"
