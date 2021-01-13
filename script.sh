c8c49776158ab6bd3f96e805a9350b389f742c7c
Dmitrii Babich <bbchd@amazon.com>
2021-01-13 19:32:59 +0100

    Update mac

--- script.sh
+++ script.sh
@@ -1,23 +1,29 @@
 # wget -O - https://raw.githubusercontent.com/dmitrybabich/asus-parental-control-switcher/master/script.sh | bash
 # sudo su -c "bash <(wget -qO- https://raw.githubusercontent.com/dmitrybabich/asus-parental-control-switcher/master/script.sh)" root
 
-# wget -O https://raw.githubusercontent.com/dmitrybabich/asus-parental-control-switcher/master/script.sh
+# rm script.sh && wget https://raw.githubusercontent.com/dmitrybabich/asus-parental-control-switcher/master/script.sh && chmod +x script.sh
 ENABLED=`nvram get MULTIFILTER_ENABLE`
 echo $ENABLED
 if [ $ENABLED = "2" ]; then
     echo "Disabling PC"
-    nvram set MULTIFILTER_ALL=""
+    nvram set MULTIFILTER_URL_ENABLE=""
+    nvram set MULTIFILTER_ALL="0"
     nvram set MULTIFILTER_MACFILTER_DAYTIME=""
+    nvram set MULTIFILTER_TMP=""
     nvram set MULTIFILTER_DEVICENAME=""
     nvram set MULTIFILTER_ENABLE=""
+    nvram set MULTIFILTER_URL=""
     nvram set MULTIFILTER_MAC=""
 else
     echo "Enabling PC"
+    nvram set MULTIFILTER_URL_ENABLE=""
     nvram set MULTIFILTER_ALL="1"
     nvram set MULTIFILTER_MACFILTER_DAYTIME="<"
+    nvram set MULTIFILTER_TMP=""
     nvram set MULTIFILTER_DEVICENAME="XBOX"
     nvram set MULTIFILTER_ENABLE="2"
-    nvram set MULTIFILTER_MAC="36:81:78:19:42:0E"
+    nvram set MULTIFILTER_MAC="30:AB:6A:7A:35:83"
+    nvram set MULTIFILTER_URL=""
 fi
 echo "Applying changes"
 
